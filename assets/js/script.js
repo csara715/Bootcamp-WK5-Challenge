c90126dc42e7b410344d872874f8ecfca4c8b390
csara <csara715@gmail.com>
2022-11-10 14:26:11 -0700

    local storage fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,65 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
-var currentDay = moment().format("MMMM Do YYYY")
 
 var currentHour = moment().format("HH");
+var eventList = localStorage.getItem("eventList");
+var idList = localStorage.getItem("idList");
 
-var eventList = JSON.parse(localStorage.getItem("eventList")) || [];
-var idList = JSON.parse(localStorage.getItem("idList")) || [];    
-
-function init() { 
-    renderSchedule();
+function init() {
+    if (eventList) {
+        eventList = JSON.parse(eventList);
+        idList = JSON.parse(idList);
+      } else {
+        eventList = [];
+        idList = [];
+      }
+    renderSchedule();  
 };
 
 //Code to color code the time blocks
-$("tr").each(function() {
-    if (this.id === currentHour) {
-        $(this).children().eq(1).addClass("present");
-    } else if (this > currentHour) {
-        $(this).children().eq(1).addClass("future");
-    } else {
-        $(this).children().eq(1).addClass("past");
-    }
-    return;
+$("tr").each(function () {
+  if (this.id === currentHour) {
+    $(this).children().eq(1).addClass("present");
+  } else if (this.id > currentHour) {
+    $(this).children().eq(1).addClass("future");
+  } else {
+    $(this).children().eq(1).addClass("past");
+  }
+  return;
 });
 
-var addEvent = function(event) {
-    event.preventDefault();
+// Add events to the schedule
+var addEvent = function (event) {
+  event.preventDefault();
 
-    var timeSlot = $(this).parent().parent().children().eq(1);
-    var eventId = $(this).parent().parent().attr('id');
-    var newEvent = timeSlot.children().val().trim();
-    eventList.push(newEvent);
-    idList.push(eventId);
-    localStorage.setItem("eventList", JSON.stringify(eventList));
-    localStorage.setItem("idList", JSON.stringify(idList));
-    
+  var timeSlot = $(this).parent().parent().children().eq(1);
+  var eventId = $(this).parent().parent().attr("id");
+  var newEvent = timeSlot.children().val().trim();
+  eventList.push(newEvent);
+  idList.push(eventId);
+  localStorage.setItem("eventList", JSON.stringify(eventList));
+  localStorage.setItem("idList", JSON.stringify(idList));
+  renderSchedule();
 };
 
+// Render the events stored in local storage to the schedule
 function renderSchedule() {
-
+  if (eventList.length === 0) {
+    return;
+  } else {
     var storedEvents = JSON.parse(localStorage.getItem("eventList"));
     var storedIds = JSON.parse(localStorage.getItem("idList"));
-    
+
     for (j = 0; j < storedIds.length; j++) {
-        $("tr").each(function() {
-            if (this.id === storedIds[j]) {
-                var textBox = $(this).children().eq(1);
-                textBox.children().append().val(storedEvents[j]);
-            }
-        });
+      $("tr").each(function () {
+        if (this.id === storedIds[j]) {
+          var textBox = $(this).children().eq(1);
+          textBox.children().append().val(storedEvents[j]);
+        }
+      });
     }
-};
+  }
+}
 
 init();
 
-$("i").on('click', addEvent);
+$("i").on("click", addEvent);
